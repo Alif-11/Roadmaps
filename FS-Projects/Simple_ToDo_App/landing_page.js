dd1065c2ba6e722d2e062116e7c39b9cd92ed66b
Alif Abdullah <alifabdullah7040@gmail.com>
2025-03-24 23:05:17 -0400

    Created popup open and close effect. Visuals are still a little rough, so I need to make the popup look prettier.

--- FS-Projects/Simple_ToDo_App/landing_page.js
+++ FS-Projects/Simple_ToDo_App/landing_page.js
@@ -1,3 +1,16 @@
-function popUpControl() {
-  popupWindow = document.getElementById("task-list")
-}
+popupWindow = document.getElementById("add-task-popup");
+popupWindowOpenButton = document.getElementById("popup-open-button");
+popupWindowCloseButton = document.getElementById("popup-close-button");
+body = document.body;
+
+popupWindowOpenButton.addEventListener("click", () => {
+  popupWindow.classList.add("popup-visible");
+  body.classList.add("popup-visible");
+  console.log("open");
+})
+
+popupWindowCloseButton.addEventListener("click", () => {
+  popupWindow.classList.remove("popup-visible");
+  body.classList.remove("popup-visible");
+  console.log("close");
+})
